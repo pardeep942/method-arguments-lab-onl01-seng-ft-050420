0a86bef944b53ae73923c27ae843be7530758cb2
PARDEEP KAUR <pkaur84@aol.com>
2020-05-05 22:16:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,5 +2,5 @@ def introduction (name)
  puts "Hi, my name is #{name}."
  
 def introduction_with_language 
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "I am learning to program in #{language}."
 end
